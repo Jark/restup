b98d53696911cc2215b96a136b28cde9fff91580
Jark Reijerink <jark.reijerink@gmail.com>
2016-07-03 07:14:39 +0100

    Added NuGet packaging for WebServer.Logging interfaces.

---
+++ scripts/BuildNuGetPackages.ps1
@@ -0,0 +1,38 @@
+<#
+
+.SYNOPSIS
+
+Builds the WebServer and WebServer.Logging project. Optionally updates the release notes in the .nuspec file.
+
+.DESCRIPTION
+
+This file is meant as an easy way to generate the restup releated packages and test them before release using the file system as a package source.
+
+The loggingVersion is optional and set to "1.0.0" since we don't expect it to change any time soon.
+
+The output can be found in the nugetoutput folder.
+
+#>
+param(
+    [Parameter(Mandatory=$true)] [string]$mainVersion,
+    [Parameter(Mandatory=$false)] [string]$loggingVersion = "1.0.0",
+    [Parameter(Mandatory=$false)] [string]$mainReleaseNotes,
+    [Parameter(Mandatory=$false)] [string]$loggingReleaseNotes
+)
+
+Set-StrictMode -Version Latest
+
+Import-Module "$PSSCriptRoot/BuildFunctions.psm1"
+
+$outputDirectory = Resolve-Path "$PSScriptRoot/../nugetoutput"
+
+Remove-Item $outputDirectory -Recurse -Force | Out-Null
+New-Item $outputDirectory -ItemType Directory -Force | Out-Null
+
+Set-NuSpecFile "$PSScriptRoot/../src/WebServer.Logging/WebServer.Logging.nuspec" $loggingVersion $loggingReleaseNotes
+Set-NuSpecFile "$PSScriptRoot/../src/WebServer/WebServer.nuspec" $mainVersion $mainReleaseNotes
+
+Invoke-NuGetPack "$PSScriptRoot/../src/WebServer.Logging/WebServer.Logging.csproj" $outputDirectory
+Invoke-NuGetPack "$PSScriptRoot/../src/WebServer/WebServer.csproj" $outputDirectory
+
+Write-Host "The NuGet packages for Restup and Restup.Logging can be found in $outputDirectory"
